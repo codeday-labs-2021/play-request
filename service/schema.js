8227b1fb9e3175469fba372e9953689351456b5b
Rohan Bansal <bansalrohan18@gmail.com>
2021-07-19 18:01:23 -0700

    add functionality to first two REST endpoints

--- service/schema.js
+++ service/schema.js
@@ -1,29 +1,58 @@
-var schema = require('js-schema');
-var flocking = require('flocking');
+class TrackData {
+    synthdata;
 
-var Synth = schema({
-    id: Number,
-    frequency : Number,
-    oscillator : String,
-});
-
-
-class Sample {
-    // id
-    // length in seconds
-    // volume
-    // music data (bytes)
+    constructor(data) {
+        this.synthdata = data.synth;
+    }
 }
 
-class Track {
-    // list of Sample objects, with corresponding timestamps
+class Project {
+    name;
+    description;
+    contributors;
+    trackdata;
+
+    constructor(data) {
+        this.name = data.name;
+        this.description = data.description;
+        this.contributors = data.contributors;
+        this.trackdata = new TrackData(data.trackdata);
+    }
+}
+
+module.exports = {
+    Project
 }
 
 
-// // LATER
-// class History {
+
+
+// var schema = require('js-schema');
+// var flocking = require('flocking');
+
+// var Synth = schema({
+//     id: Number,
+//     frequency : Number,
+//     oscillator : String,
+// });
+
+
+// class Sample {
+//     // id
+//     // length in seconds
+//     // volume
+//     // music data (bytes)
 // }
 
-// class PlayRequest {
+// class Track {
+//     // list of Sample objects, with corresponding timestamps
 // }
+
+
+// // // LATER
+// // class History {
+// // }
+
+// // class PlayRequest {
+// // }
 
--- service/server.js
+++ service/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-
+app.use(express.json());
 app.use(require('./endpoints'));
 
 
