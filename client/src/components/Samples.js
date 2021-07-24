2281fd57b7ba1d3e080e80e989d55c286428eb76
Rohan Bansal <bansalrohan18@gmail.com>
2021-07-24 14:20:38 -0700

    finished adding fetch api

--- client/src/components/Samples.js
+++ client/src/components/Samples.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import './Samples.css'
+import WithListLoading from './WhileSampleListLoading'
+import SampleList from './SampleList'
+import axios from 'axios'
 
 function Samples() {
-  return <span>Samples</span>;
+
+    const ListLoading = WithListLoading(SampleList);
+    const [appState, setAppState] = useState({
+      loading: false,
+      samples: null,
+    });
+
+    useEffect(() => {
+      setAppState({ loading: true });
+      const apiUrl = `http://localhost:4000/project/607eb708-61a6-4cae-aca2-3f789a53dbdf/samples/`;
+      const getSamples = async () => {
+        let { data } = await axios.get(apiUrl);
+        setAppState({ loading: false, samples: data });
+        console.log(data);  
+      }
+      getSamples();
+
+    }, [setAppState]);
+
+    return(
+      <div>
+        <h1 id="samples-title">Samples</h1>
+        <ListLoading isLoading={appState.loading} samples={appState.samples} />
+      </div>
+    );
 }
 
 export default Samples;
--- service/server.js
+++ service/server.js
@@ -3,6 +3,9 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+var cors = require('cors')
+
+app.use(cors());
 app.use(express.json());
 app.use(require('./endpoints'));
 
