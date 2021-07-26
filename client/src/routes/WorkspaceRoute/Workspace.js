b938b6ef270a0880c00e93f8b41e983c5b7869d5
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-07-27 04:27:57 +0900

    feat: Add movable (draggable) components

--- client/src/routes/WorkspaceRoute/Workspace.js
+++ client/src/routes/WorkspaceRoute/Workspace.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Chatroom from "../../components/WorkspacePanels/ChatroomPanel/Chatroom";
-import Samples from "../../components/WorkspacePanels/SamplesPanel/Samples";
 import Effects from "../../components/WorkspacePanels/EffectsPanel/Effects";
 import MainWorkspace from "../../components/WorkspacePanels/MainWorkspacePanel/MainWorkspace";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 import "./Workspace.css";
+
+const samplesList = [
+  {
+    id: 1,
+    title: "Sample 1",
+  },
+  {
+    id: 2,
+    title: "Sample 2",
+  },
+  {
+    id: 3,
+    title: "Sample 3",
+  },
+  {
+    id: 4,
+    title: "Sample 4",
+  },
+  {
+    id: 5,
+    title: "Sample 5",
+  },
+];
 
 function Workspace() {
   return (
@@ -20,7 +43,40 @@ function Workspace() {
         </div>
         <div className="wrapper__column">
           <div className="column_samples">
-            <Samples />
+            <DragDropContext>
+              <Droppable droppableId="samples">
+                {(provided) => (
+                  <ul
+                    className="samples"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {samplesList.map(({ id, title }, index) => {
+                      return (
+                        <Draggable
+                          key={id}
+                          draggableId={id.toString()}
+                          index={index}
+                        >
+                          {(provided) => (
+                            <li
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              ref={provided.innerRef}
+                            >
+                              <div>
+                                <span>{title}</span>
+                              </div>
+                            </li>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            </DragDropContext>
           </div>
           <div className="column_soundeffects">
             <Effects />
